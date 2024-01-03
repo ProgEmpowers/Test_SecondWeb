353a7f8fa93bcd7803a83f781d58d445b55bd204
sashi <sashi@LAPTOP-8EJEM9Q0>
2024-01-03 22:32:32 +0530

    Add project files.

---
+++ SecondWeb/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecondWeb.Models;
+using System.Globalization;
+
+namespace SecondWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Authenticated wih JWT";
+        }
+        [HttpGet]
+        [Route("Details")]
+        public string Details()
+        {
+            return "Authenticated wih JWT";
+        }
+        [Authorize]
+        [HttpPost]
+
+        public string AddUser(Users user)
+        {
+            return "User added with username " + user.Username;
+        }
+    }
+}
